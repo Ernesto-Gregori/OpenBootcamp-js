8259268c7ffa47aa2f7151b00731db363bd8a354
Oscar Ernesto Gregori Gallardo <46912758+Oscar951@users.noreply.github.com>
2022-09-13 06:55:48 -0600

    Crear una lista

---
+++ 2-ejercicio-sintaxis/perfil.js
@@ -0,0 +1,14 @@
+const perfil = [
+    "Oscar Gregori", 
+    26, 
+    true, 
+    new Date(1995, 11, 4), 
+    {
+        titulo: "Clean Code: A Handbook of Agile Software Craftsmanship",
+        autor: "Robert C. Martin",
+        fecha: 2008,
+        url: `https://www.amazon.com/Clean-Code-Handbook-Software-Craftsmanship/dp/0132350882`
+    }
+]
+
+console.log(perfil)
